6fe03ec8a38101565797a758b5b7562c8c3bca31
Anushray Gupta <anushray7@gmail.com>
2015-10-31 14:21:49 +0530

    chaned static_root to blah

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/blah/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
 
 AUTH_USER_MODEL = 'account.MyUser'
