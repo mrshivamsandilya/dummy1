84649039dbb095299b02a44e44379d8a7a7c7f65
Anushray Gupta <anushray7@gmail.com>
2015-10-31 13:48:12 +0530

    Added DB settings

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'newproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sometemp',
+        'HOST': 'localhost',
+        'USER' : 'root',
+        'PASSWORD': 'anushray',
+        'PORT' : 3306
     }
 }
 
